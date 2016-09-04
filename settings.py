2f23033fad5f54df24a8ac444b969bbc3050c63b
Win10 <mrkhchan@outlook.com>
2016-09-04 12:36:20 +0800

    bullet

--- settings.py
+++ settings.py
@@ -4,3 +4,10 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (133, 193, 255)
         self.ship_speed_factor = 1.5
+        
+        # bullet
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 5
