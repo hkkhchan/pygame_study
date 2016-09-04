a3a3ac3644b03b89229af99403df81f67b35ffcf
Win10 <mrkhchan@outlook.com>
2016-09-04 12:43:04 +0800

    bullet

--- game_functions.py
+++ game_functions.py
@@ -6,9 +6,8 @@ def check_keydown_events(event, ai_settings,screen,ship,bullets):
         ship.moving_right= True
     elif event.key == pygame.K_LEFT:
         ship.moving_left= True
-    elif event.key == pygame.K_SPACE and len(bullets) < ai_settings.bullets_allowed:
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -31,3 +30,14 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
     ship.blitme()
     pygame.display.flip()
+
+def update_bullets(bullets):
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
