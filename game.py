4398a8a1e8fd1261ee4b9a46415cbb5eb706f572
hkkhchan <hkkhchan@gmail.com>
2016-09-08 22:11:45 +0800

    add play button

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from stats import GameStats
+from button import Button
 from ship import Ship
 from alien import Alien
 import game_functions as gf
@@ -10,6 +11,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    play_button = Button(ai_settings, screen, "Play")
     stats = GameStats(ai_settings)
     ship = Ship(ai_settings, screen)
     bullets = Group()
@@ -21,6 +23,6 @@ def run_game():
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets,play_button)
 run_game()
 
--- game_functions.py
+++ game_functions.py
@@ -31,13 +31,17 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets,play_button):
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+    
+    if not stats.game_active:
+        play_button.draw_button()
     pygame.display.flip()
+    
 
 def update_bullets(ai_settings, screen, ship, aliens, bullets):
     bullets.update()
