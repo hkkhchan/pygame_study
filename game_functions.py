8174858b4f89c25f4fb4450a9ed1e93e87743936
hkkhchan <hkkhchan@gmail.com>
2016-09-08 22:52:06 +0800

    mouse

--- game_functions.py
+++ game_functions.py
@@ -37,6 +37,7 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        ai_settings.initialize_dynamic_settings()
         pygame.mouse.set_visible(False)
         stats.reset_stats()
         stats.game_active = True
@@ -76,6 +77,7 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     
     if len(aliens) == 0:
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
         
 def get_number_aliens_x(ai_settings, alien_width):
--- settings.py
+++ settings.py
@@ -14,8 +14,19 @@ class Settings():
         self.bullets_allowed = 5
         
         # alien
-        self.alien_speed_factor = 3
         self.fleet_drop_speed = 10
+        
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+        
+    def self.initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
         self.fleet_direction = 1
         
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
 
