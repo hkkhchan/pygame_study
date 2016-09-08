782389c15c63d6e5328f9338b2803689d31e0965
hkkhchan <hkkhchan@gmail.com>
2016-09-08 22:27:15 +0800

    add play button

--- game.py
+++ game.py
@@ -18,11 +18,11 @@ def run_game():
     aliens = Group()
     gf.create_fleet(ai_settings, screen, ship, aliens)
     while True:
-        gf.check_events(ai_settings,screen,ship,bullets)
+        gf.check_events(ai_settings,screen,stats, play_button, ship,bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets,play_button)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 run_game()
 
