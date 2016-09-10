0358e0bc88c5e93ea66f55a1cd0fa7b5ec16a04d
hkkhchan <hkkhchan@gmail.com>
2016-09-10 23:19:21 +0800

    level

--- game_functions.py
+++ game_functions.py
@@ -86,6 +86,8 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
     if len(aliens) == 0:
         bullets.empty()
         ai_settings.increase_speed()
+        stats.level += 1
+        sb.prep_level()
         create_fleet(ai_settings, screen, ship, aliens)
         
 def get_number_aliens_x(ai_settings, alien_width):
--- scoreboard.py
+++ scoreboard.py
@@ -11,6 +11,7 @@ class Scoreboard():
         self.font = pygame.font.SysFont(None, 48)
         
         self.prep_score()
+        self.prep_level()
         
     def prep_score(self):
         rounded_score = int(round(self.stats.score, -1))
@@ -21,5 +22,13 @@ class Scoreboard():
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
         
+    def prep_level(self):
+        self.level_image = self.font.render(str(self.stats.level),True, 
+            self.text_color, self.ai_settings.bg_color)
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+        
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
